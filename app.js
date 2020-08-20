ffd42a47d48ea7cf939582dcd0885f09de1ce792
Anthony Bergen <50747592+adbergen@users.noreply.github.com>
2020-08-20 09:40:25 -0400

    added templates for all roles and linked to app.js

--- app.js
+++ app.js
@@ -1,14 +1,136 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+var validator = require("email-validator");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const outputPath = path.join(OUTPUT_DIR, "main.html");
 
 const render = require("./lib/htmlRenderer");
+const team = [];
+
+function addMore() {
+  inquirer.prompt([{
+    type: "list",
+    choices: ["Yes", "No"],
+    message: "Would you like to add more team members?",
+    name: "addmore",
+  }]).then(function (response) {
+    this.response = response.addmore;
+    if (this.response === "Yes") {
+      return askQuestions();
+    } else {
+      if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
+      }
+      fs.writeFile(outputPath, render(team), "utf8", function (error) {
+        if (error) {
+          console.log(error);
+          return;
+        }
+      });
+      return console.log("Success. HTML has been created. Please navigate to the output folder to view your results")
+    }
+  });
+}
+
+
+
+function askQuestions() {
+
+  inquirer.prompt([
+    {
+      type: "list",
+      choices: ["Manager", "Engineer", "Intern"],
+      message: "What is your role in the company?",
+      name: "role"
+    },
+    {
+      type: "input",
+      message: "What is your full name?",
+      name: "name"
+    },
+    {
+      type: "input",
+      message: "What is your ID number?",
+      name: "id"
+    },
+  ]).then(function (response) {
+    this.name = response.name;
+    this.id = response.id;
+    this.role = response.role;
+
+    function checkEmail() {
+      inquirer.prompt([
+        {
+          type: "email",
+          message: "What is your email address",
+          name: "email"
+        }
+      ]).then(function (response) {
+        this.email = response.email;
+        if (validator.validate(this.email)) {
+          this.email = response.email;
+          if (this.role === "Manager") {
+            inquirer.prompt([
+              {
+                type: "input",
+                message: "What is your office number?",
+                name: "officeNumber"
+              }
+            ]).then(function (response) {
+              this.officeNumber = response.officeNumber;
+              const newManager = new Manager(this.name, this.id, this.email, this.officeNumber);
+              console.log("Success. The team has a new manager.");
+              team.push(newManager);
+            }).then(function () {
+              addMore();
+            });
+          } else if (this.role === "Engineer") {
+            inquirer.prompt([
+              {
+                type: "input",
+                message: "What is your gitHub username?",
+                name: "gitHub"
+              }
+            ]).then(function (response) {
+              this.gitHub = response.gitHub;
+              const newEngineer = new Engineer(this.name, this.id, this.email, this.gitHub);
+              console.log("Success. The team has a new engineer.");
+              team.push(newEngineer);
+            }), then(function () {
+              addMore();
+            });
+          } else if (this.role === "Intern") {
+            inquirer.prompt([
+              {
+                type: "input",
+                message: "What college did you attend?",
+                name: "school"
+              }
+            ]).then(function (response) {
+              this.school = response.school;
+              const newIntern = new Intern(this.name, this.id, this.email, this.school);
+              console.log("Success. The team has a new intern.");
+              team.push(newIntern);
+            }).then(function () {
+              addMore();
+            });
+          };
+        } else {
+          console.log("Please enter a valid email address.");
+          checkEmail();
+        }
+      })
+    };
+    checkEmail();
+  });
+};
+askQuestions();
 
 
 // Write code to use inquirer to gather information about the development team members,
